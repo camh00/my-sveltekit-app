d1653e4d3078fabf763836e2a2137b767d6ecbcb
Cameron Hancock <cameron.hancock03@hotmail.com>
2024-09-16 18:20:02 -0600

    Added Nav template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,20 @@
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
 		<h1 class="h1" >Let's get cracking bones!</h1>
+		
+		<nav class="list-nav">
+			<!-- (optionally you can provide a label here) -->
+			<ul>
+				<li>
+					<a href="/elements/lists">
+						<span class="badge bg-primary-500">(icon)</span>
+						<span class="flex-auto">Skeleton</span>
+					</a>
+				</li>
+				<!-- ... -->
+			</ul>
+		</nav>
+
 		<p>Start by exploring:</p>
 		<ul>
 			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
