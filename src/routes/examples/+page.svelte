f64cd0a1ba28469c4b2405d8d7caa697fd9084d0
Cameron Hancock <cameron.hancock03@hotmail.com>
2024-09-20 10:48:15 -0600

    Scrapped aside and added more js

--- src/routes/examples/+page.svelte
+++ src/routes/examples/+page.svelte
@@ -22,17 +22,26 @@
 			</ul>
 		</nav>
         <hr class="!border-t-4" />
-		<aside class="absolute w-half h-full bg-surface-50-900-token border-r-2 shadow-lg">
-			<nav class="p-12 text-xl">
-				<a class="block" href="#about">About</a>
-				<a class="block" href="#contact">Contact</a>
-			</nav>
-		</aside>
+		
 
         <h3 class="h3 ">Here are some great examples of JavaScript That I have been learning!</h3>
         <CodeBlock language="js" class="text-left" code={'console.log(\'This is great!\');'}></CodeBlock>
-		<h3 class="h3 ">Objects</h3>
-		<p>Objects are a great way to organize and store date and javascripts simple approach makes building object simple.</p>
-		<CodeBlock language="js" class="text-left" lineNumbers code={'const car = {\n	type:"Fiat",\n	model:"500",\n	color:"white"\n};'}></CodeBlock>
+		<div class="border-solid border-2 rounded-3xl p-4">
+			<h3 class="h3 ">Objects</h3>
+			<p>Objects are a great way to organize and store date and javascripts simple approach makes building object simple.</p>
+			<CodeBlock language="js" class="text-left" lineNumbers code={'const car = {\n	type:"Fiat",\n	model:"500",\n	color:"white"\n};'}></CodeBlock>
+			<p>You can then change or add propperties of an object using dot notation</p>
+			<CodeBlock language="js" class="text-left" lineNumbers code={'car.color = "red";\ncar.owner = "John Doe";'}></CodeBlock>
+		</div>
+		<hr class="!border-t-4" />
+
+		<div class="border-solid border-2 rounded-3xl p-4">
+			<h3 class="h3">Array Manipulation</h3>
+			<p>There are some great functions made to manipulate data in arrays.</p>
+			<p>For example, splice() is a great method to remove and add data into an array.</p>
+			<CodeBlock language="js" class="text-left" lineNumbers code={'\/\/ Create an Array\nconst dogNames = ["Spot", "Fred", "Rover", "Lassy"];\n\n\/\/ At position 2, remove 1 item, add "Rusty" and "Marshal"\ndogNames.splice(2, 1, "Rusty", "Marshal");'}></CodeBlock>
+			<p>Another great tool is the slice() method. While you can use slice to select sections of a string, you can also select a section of an array without changing the array</p>
+			<CodeBlock language="js" class="text-left" lineNumbers code={'\/\/ Create an Array\nconst dogNames = ["Spot", "Fred", "Rover", "Lassy"];\n\n\/\/ Print out the 2nd and 3rd names\nconsole.log(dogNames.slice(1,3));'}></CodeBlock>
+		</div>
 	</div>
 </div>
