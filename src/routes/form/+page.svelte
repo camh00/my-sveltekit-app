adc1a879bbc9de5e4fbd2cb4276db5217cf1a9a1
camh00 <cameron.hancock03@hotmail.com>
2024-10-23 15:43:03 -0600

    added forms page

---
+++ src/routes/form/+page.svelte
@@ -0,0 +1,37 @@
+<div class="container h-full mx-auto flex justify-center items-center text-center">
+	<div class="space-y-5">
+		<h1 class="h1" >Coding Examples</h1>
+		
+		<nav class="nav btn-group">
+			<!-- (optionally you can provide a label here) -->
+			<ul>
+				<li>
+					<a href="/" class="bg-primary-500">
+						<span>🏠</span>
+						<span class="flex-auto">Home</span>
+					</a>
+					<a href="/examples" class="bg-primary-500">
+						<span>🖥️</span>
+						<span class="flex-auto">Examples</span>
+					</a>
+					<a href="/pokemon" class="bg-primary-500">
+						<span>🎮</span>
+						<span class="flex-auto">Pokemon</span>
+					</a>
+				</li>
+			</ul>
+		</nav>
+        <hr class="!border-t-4" />
+		
+        <form method="POST">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            <label for="message">Message:</label>
+            <textarea id="message" name="message" required></textarea>
+            <button type="submit">Submit</button>
+        </form>
+
+	</div>
+</div>
